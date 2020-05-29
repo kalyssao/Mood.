f92bbd8bee9b1ec3614197c97ba582ad4f63baff
kalyssao <klysssa@icloud.com>
2020-05-29 12:20:27 +0000

    removed reference to store

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import store from "./store";
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
@@ -10,7 +9,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
   router,
   render: (h) => h(App),
 }).$mount('#app');
