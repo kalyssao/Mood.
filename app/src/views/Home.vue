152170dc83b9949a5ac7d54622a49b0b9707388b
kalyssao <klysssa@icloud.com>
2020-05-31 21:30:29 +0000

    restructured ish - on to finalizing model et voila

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -4,7 +4,8 @@
       <b-col md="1"> </b-col>
       <b-col md="5">
         <RwvCamera @moodPredicted="setMood($event)"/>
-      </b-col>
+        <b-button squared variant="dark" id="snap" v-on:click="getEmotion()"> Snap.</b-button>
+        </b-col>
       <b-col md="1"></b-col>
       <b-col md="4">
         <b-row>
@@ -21,32 +22,53 @@
 
 <script>
 // @ is an alias to /src
-import axios from 'axios'
 import RwvCamera from '@/components/TheCamera.vue';
 import RwvText from '@/components/TheText.vue';
-import RwvRecommendations from '@/components/TheRecommendations.vue'
+import RwvRecommendations from '@/components/TheRecommendations.vue';
+
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenetModule from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
 
 export default {
-  name: 'Home',
-  data () {
-    return {
-      mood: ""
+    name: 'Home',
+    data () {
+        return {
+            mood: "",
+            classifier: null,
+            mobilenet: null,
+            class: null,
+            playlists: []
+        }
+    },
+    components: {
+        RwvCamera,
+        RwvText,
+        RwvRecommendations
+    },
+    mounted() {
+        this.init()
+    },
+    methods: {
+        async init(){
+            // load the load mobilenet and create a KnnClassifier
+            this.classifier = knnClassifier.create();
+            this.mobilenet = await mobilenetModule.load();
+        },
+        async getEmotion() {
+            console.log('getting emotion..')
+            /*const img = tf.FromPixels(this.$children[0].webcam.webcamElement)
+            const logits = this.mobilenet.infer(img, 'conv_preds')
+            const pred = await this.classifier.predictClass(logits)
+            this.detected_e = this.emotions[pred.classIndex];
+            this.setMood(detected_e)*/
+            this.setMood('happy')
+        },
+        setMood(mood) {
+            console.log('mood detected, ', mood)
+            var self = this
+            this.mood = mood
+        }
     }
-  },
-  components: {
-    RwvCamera,
-    RwvText,
-    RwvRecommendations
-  },
-  methods: {
-    setMood(mood) {
-      this.mood = mood
-    }
-  },
-  watch: {
-    mood: function () {
-      console.log('mood determined.')
-    }
-  }
 };
 </script>
