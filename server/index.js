0f05d0d5f77e63dcea2af56f7e4ad94fdebe2ce8
kalyssao <klysssa@icloud.com>
2020-06-01 01:34:16 +0000

    added models - having issues with expressions; might have to change spotify back also

--- server/index.js
+++ server/index.js
@@ -6,23 +6,28 @@ app.use((req, res, next) => {
     next();
 });
 
-var SpotifyWebApi = require('spotify-web-api-node')
-
 var client_id = '9d583449efba428abdccef4978f3f87a'
 var client_secret = '70629c95044a4d79901a0ff48a87d9e5'
 
+var SpotifyWebApi = require('spotify-web-api-node')
 var spotifyApi = new SpotifyWebApi({
     clientId: client_id,
-    clientSecret: client_secret
+    clientSecret: client_secret,
+    redirectUri: 'localhost:8080/#/home'
 });
 
-spotifyApi.clientCredentialsGrant()
+function newToken() {
+    spotifyApi.clientCredentialsGrant()
     .then(function(data) {
         spotifyApi.setAccessToken(data.body['access_token']);
     })
     .catch(function(err) {
         console.log('Something went wrong when retrieving an access token', err.message);
     })
+}
+
+newToken();
+tokenRefreshInterval = setInterval(newToken, 1000 * 60 * 60);
 
 app.get('/recommend', function(request, response) {
     // from when the mood is sent via the payload
