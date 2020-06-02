fa6bf3aec3f8fa35750c110258168da4574d3af2
kalyssao <klysssa@icloud.com>
2020-06-02 08:24:17 +0000

    initialised serverless, working on expression bugs

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -29,13 +29,19 @@ import RwvRecommendations from '@/components/TheRecommendations.vue';
 import * as tf from '@tensorflow/tfjs';
 import * as faceapi from 'face-api.js';
 
+const params = {
+    minConfidence: 0.5
+}
+
 export default {
     name: 'Home',
     data () {
         return {
             mood: "",
             class: null,
-            playlists: []
+            playlists: [],
+            faceModel: null,
+            emotionModel: null
         }
     },
     components: {
@@ -48,19 +54,37 @@ export default {
     },
     methods: {
         async init(){
-            // load the face detection api
-            await faceapi.loadSsdMobilenetv1Model('/models')
-            await faceapi.loadFaceExpressionModel('/models')
+            var self = this
+
+            // load the face detection api & emotion detection model
+            await faceapi.loadSsdMobilenetv1Model('/models/features/')
+            await faceapi.loadFaceExpressionModel('/models/features')
+            
+            this.emotionModel = await tf.loadLayersModel('/models/emotion/model.json')
+            
         },
         async getEmotion() {
-            console.log('getting face from face api..')
-            const img = tf.browser.fromPixels(this.$children[0].webcam.webcamElement)
-            const detectionsWithExpressions = await faceapi.detectSingleFace(img)
             
+            var self = this
+            console.log('getting face from face api..')
+
+            //const img = tf.browser.fromPixels(this.$children[0].webcam.webcamElement)
+            const img = tf.browser.fromPixels(this.$children[0].webcam.webcamElement)
+            console.log(img)
+            //const input = await faceapi.toNetInput(img, false, true)
+            const detectionsWithExpressions = await faceapi.detectSingleFace(img).withFaceExpressions()
             console.log(detectionsWithExpressions)
-            //this.detected_e = this.emotions[pred.classIndex];
-            //this.setMood(detected_e)*/
-            this.setMood('happy')
+            /*const detections = await faceapi.ssdMobilenetv1(input, params)
+            //const detections = results.map(r => r.faceDetection)
+            console.log(input)
+
+            const face = await faceapi.extractFaces(input.inputs[0], detections)
+            console.log(face)
+            
+            const output = await this.emotionModel.classify(face)
+            console.log(output)
+            //this.setMood('happy')*/
+            
         },
         setMood(mood) {
             console.log('mood detected, ', mood)
